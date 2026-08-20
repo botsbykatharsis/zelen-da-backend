e27de9eebffb6c850b48bdf35f2a16ebd4124fd9
Ekaterina <you@email.com>
2026-08-20 13:31:46 +0300

    update design

--- models/order.py
+++ models/order.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 class OrderItem(BaseModel):
     id: int
     qty: int
 
 class Order(BaseModel):
-    user_id: int
+    user_id: Optional[int] = None
+    username: Optional[str] = "unknown"
     items: List[OrderItem]
     name: str
     phone: str
